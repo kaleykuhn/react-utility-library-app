8dc7b2a7f6ae2b23e55e993e8c2dd57e17f3b8db
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-14 19:27:14 -0700

    add changes to import and export,render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style/master.scss"; //applies global scss styles
+import { uiData } from "./data/ui";
 
-function App() {
-   return (
-      <div>
-         <h1 className="text-primary">Hello World</h1>
-      </div>
-   );
+// this is a pre-built class given name App adding into component before render
+export default class App extends Component {
+   constructor() {
+      super();
+      console.log(uiData);
+   }
+
+   // after render
+   render() {
+      const renderInputs = (num) => {
+         const inputs = [];
+         for (let i = 0; i < num; i++) {
+            //render some JSX
+            inputs.push(
+               <input type="text" className="form-control inline-action" />
+            );
+         }
+         return inputs;
+      };
+
+      return (
+         <div className="container">
+            <div className="row">
+               {uiData.map((component) => {
+                  return (
+                     <div className="col-12 col-lg-8 offset-lg-2 mb-5">
+                        <p className="name">
+                           <b>{component.name}</b>&nbsp;-&nbsp;{component.desc}
+                        </p>
+                        <pre style={{ display: "none" }}>
+                           <code></code>
+                        </pre>
+                        <div className="actions float-right">
+                           {renderInputs(component.inputs)}
+
+                           <button className="btn btn-primary inline-action">
+                              Run
+                           </button>
+                        </div>
+                        <div className="clearfix mb-3"></div>
+                        <div
+                           className="alert alert-primary"
+                           style={{ display: "none" }}
+                        ></div>
+                        <div
+                           className="alert alert-danger"
+                           style={{ display: "none" }}
+                        ></div>
+                     </div>
+                  );
+               })}
+            </div>
+         </div>
+      );
+   }
 }
-
-export default App;
