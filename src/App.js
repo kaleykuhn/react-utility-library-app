2fca2b3dedb79a5d5e68eca76c714a946907bca7
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-15 02:54:43 -0700

    add FunctionUI return

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style/master.scss"; //applies global scss styles
 import { uiData } from "./data/ui";
+import FunctionUI from "./components/FunctionUI";
 
 // this is a pre-built class given name App adding into component before render
-export default class App extends Component {
+export default class App extends React.Component {
    constructor() {
       super();
       console.log(uiData);
@@ -11,46 +12,16 @@ export default class App extends Component {
 
    // after render
    render() {
-      const renderInputs = (num) => {
-         const inputs = [];
-         for (let i = 0; i < num; i++) {
-            //render some JSX
-            inputs.push(
-               <input type="text" className="form-control inline-action" />
-            );
-         }
-         return inputs;
-      };
-
       return (
          <div className="container">
             <div className="row">
-               {uiData.map((component) => {
+               {uiData.map((functionUI) => {
                   return (
-                     <div className="col-12 col-lg-8 offset-lg-2 mb-5">
-                        <p className="name">
-                           <b>{component.name}</b>&nbsp;-&nbsp;{component.desc}
-                        </p>
-                        <pre style={{ display: "none" }}>
-                           <code></code>
-                        </pre>
-                        <div className="actions float-right">
-                           {renderInputs(component.inputs)}
-
-                           <button className="btn btn-primary inline-action">
-                              Run
-                           </button>
-                        </div>
-                        <div className="clearfix mb-3"></div>
-                        <div
-                           className="alert alert-primary"
-                           style={{ display: "none" }}
-                        ></div>
-                        <div
-                           className="alert alert-danger"
-                           style={{ display: "none" }}
-                        ></div>
-                     </div>
+                     <FunctionUI
+                        name={functionUI.name}
+                        desc={functionUI.desc}
+                        inputs={functionUI.inputs}
+                     />
                   );
                })}
             </div>
