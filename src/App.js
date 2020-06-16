7369f636f25921aefea2aa7b964bc73db0721273
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-15 23:25:51 -0700

    add input search functions,  buttons and select

--- src/App.js
+++ src/App.js
@@ -24,13 +24,62 @@ export default class App extends React.Component {
       return (
          <div className="container">
             <div className="row">
-               <div className="col-12">
+               <div className="col-12 col-lg-8 offset-lg-2 mb-5">
                   <h1 className="d-flex justify-content-center">
-                     JavaScript Functions: Basic
+                     JavaScript Utility Library
                   </h1>
                   <p className="text-center lead mb-4">
                      {getFunctionsNum()}&nbsp;functions documented
                   </p>
+
+                  <div className="custom-control custom-radio custom-control-inline">
+                     <input
+                        type="radio"
+                        id="viewMode-all"
+                        name="viewMode"
+                        className="custom-control-input"
+                     />
+                     <label
+                        className="custom-control-label"
+                        htmlFor="viewMode-all"
+                     >
+                        Show all
+                     </label>
+                  </div>
+                  <div className="custom-control custom-radio custom-control-inline ">
+                     <input
+                        type="radio"
+                        id="viewMode-favorites"
+                        name="viewMode"
+                        className="custom-control-input"
+                     />
+                     <label
+                        className="custom-control-label"
+                        htmlFor="viewMode-favorites"
+                     >
+                        Favorites
+                     </label>
+                  </div>
+                  <div className="row mt-3">
+                     <div className="col-6">
+                        <input
+                           type="text"
+                           className="form-control"
+                           placeholder="Search all functions"
+                           aria-label="Search all functions"
+                           aria-describedby="search-input"
+                           id="search-input"
+                        />
+                     </div>
+                     <div className="col-6">
+                        <select className=" form-control ">
+                           <option>Most recent</option>
+                           <option>Oldest</option>
+                           <option>A - Z</option>
+                           <option>Z - A</option>
+                        </select>
+                     </div>
+                  </div>
                </div>
                {orderedData.map((functionUI) => {
                   const { name, desc, inputs } = functionUI;
