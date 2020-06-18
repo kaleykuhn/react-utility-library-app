057b354e897477801b888e325cd501e679549be8
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-18 12:42:42 -0700

    add export helper functions convert data type

--- src/components/FunctionUI.js
+++ src/components/FunctionUI.js
@@ -1,5 +1,6 @@
 import React from "react";
 import utils from "../utils/utils";
+import { convertDataType, isObject } from "../utils/helpers";
 
 export default function FunctionUI(props) {
    console.log(utils.add(4, "pizza"));
@@ -7,6 +8,7 @@ export default function FunctionUI(props) {
    const renderInputs = (num) => {
       const inputs = [];
       for (let i = 0; i < num; i++) {
+         // input-subtract-0
          const id = `input-${props.name}-${i}`; //unique string id made with this string literal const iteration
          //render some JSX
          inputs.push(
@@ -21,6 +23,24 @@ export default function FunctionUI(props) {
 
       return inputs;
    };
+   function getUserInput(e) {
+      console.log(props.name);
+      const inputValues = [];
+      for (let i = 0; i < props.inputs; i++) {
+         const element = document.getElementById(`input-${props.name}-${i}`);
+         const value = element.value;
+         const convertedInputValues = convertDataType(value); // reason in 4 state happen to every input
+         inputValues.push(convertedInputValues); // vscode used function defined elswhere vs auto imported
+      }
+      console.log(inputValues);
+      console.log(
+         isObject({ firstName: "Mike" }), //true
+         isObject({}), //true
+         isObject([2, 3]), //false
+         isObject(null), //false
+         isObject(() => true) //false
+      );
+   }
 
    return (
       <div className="col-12 col-lg-8 offset-lg-2 mb-5">
@@ -33,7 +53,12 @@ export default function FunctionUI(props) {
          <div className="actions float-right">
             {renderInputs(props.inputs)}
 
-            <button className="btn btn-primary inline-action">Run</button>
+            <button
+               className="btn btn-primary inline-action"
+               onClick={() => getUserInput()}
+            >
+               Run
+            </button>
          </div>
          <div className="clearfix mb-3"></div>
          <div className="alert alert-primary" style={{ display: "none" }}></div>
---
+++ src/utils/helpers.js
@@ -0,0 +1,40 @@
+const safelyParseJSON = (str) => {
+   // makes sure JSON dosnt throw error trycatchblock
+   try {
+      JSON.parse(str); //always str when parsing json
+   } catch (err) {
+      return str; // give up return str
+   }
+   return JSON.parse(str); //could be undefined!
+};
+
+const isObject = (value) => {
+   return (
+      typeof value === "object" &&
+      value !== null &&
+      Array.isArray(value) === false
+   );
+   // test if something is an object
+   //if (typeof value === "object" && value!== null && Array.isArray(value) === false) {
+   //return true
+   //} else {
+   // return false;
+
+   //return  typeof yourVariable === 'object' && yourVariable !== null
+};
+
+const convertDataType = (str) => {
+   if (str === "null") return null;
+   if (str === "undefined") return undefined;
+   if (str === "true") return true;
+   if (str === "false") return false; // strictly false
+   // eslint-disable-next-line
+   if (str == Number(str)) return Number(str); // == check values but type dont matter return just num
+   const parsedJSON = safelyParseJSON(str);
+   if (Array.isArray(parsedJSON)) return parsedJSON; // check if its an array parse
+   if (isObject(parsedJSON)) return parsedJSON; // inwards and og out func withing func within func
+   return str;
+};
+// u can just to if without else evaluate in order one at a time if it doesnt happen moves on with str
+
+export { isObject, convertDataType, safelyParseJSON }; //syntax for named things
